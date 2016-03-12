1afdfac145c29159e5abe247c66f34c954bf292f
Bencaddyro <benthedah@hotmail.fr>
2016-03-13 00:29:47 +0100

    MAJ 1
    Début du travail sur O3D:
    -Création du parallelepipede simple (doublons au niveaux des sommets à optimiser plus tard)
    -Fonction de rotation du solide (à re-modifier avec le centre de rotation) (test non concluant)
    
    A faire en priorité : algo du peintre pour éviter superposition et avoir rendu fidèle
    -implementer d'autre forme simple : sphère, cylindre, pyramide, tétraèdre etc...

--- prog.c
+++ prog.c
@@ -8,8 +8,8 @@
 #include "debug.h"
 
 //#define T2D
-#define T3D
-//#define O3D
+//#define T3D
+#define O3D
 
 int main(int argc,char** argv)
 {
@@ -25,19 +25,17 @@ int main(int argc,char** argv)
 
 #ifdef T3D
   t_point3d *p10 = definirPoint3d(10,50,100), *p20 = definirPoint3d(100,240,100), *p30 = definirPoint3d(50,300,100);
-
   t_point3d *p11 = definirPoint3d(0,0,0), *p21 = definirPoint3d(30,30,0), *p31 = definirPoint3d(30,0,0);
   t_point3d *pt=definirVecteur3d(10,10,10);
-  printf("bula1\n");
   t_triangle3d *t10 = definirTriangle3d(p10, p20, p30);
   t_triangle3d *t20 = definirTriangle3d(p11, p21, p31);
-  printf("bula2\n");
 #endif
 
 #ifdef O3D
   t_point3d *origine = definirPoint3d(0,0,0), *vecteur;
-  t_objet3d *o10 = sphere_amiga(80, 8, 16);
-  rotationObjet3d(o10, origine, 90, 0,0);
+  t_objet3d *cube=parallelepipede(100,50,200);
+  //t_objet3d *o10 = sphere_amiga(80, 8, 16);
+  //rotationObjet3d(o10, origine, 90, 0,0);
 #endif
 
   int i=0;
@@ -46,32 +44,24 @@ int main(int argc,char** argv)
   timestart = SDL_GetTicks();
 
 
-  while(i<10)
+  while(i<50)
     {
       effacerFenetre(surface, 0);
 
 #ifdef T2D
-      /*
-       * etape 1 : ce n'est qu'un exemple... il faut desactiver le define T2D pour la suite
-       */
       remplirTriangle2d(surface, t1, echelle_de_couleur(30*i));
-      SDL_Delay(1);
+      SDL_Delay(500);
 #endif
-      //printf("bula3\n");
+      
 #ifdef T3D
 
-      //printf("bula4\n");
- 
-      rotationTriangle3d(t10, p10, 0, 10, 0); // rotation d'axe Y
-      translationTriangle3d(t20,pt);
-      //printf("bula5\n");
-     
+      rotationTriangle3d(t10, p10, 0, 10, 0);
+      translationTriangle3d(t20,pt);     
       //remplirTriangle3d(surface, t10, echelle_de_couleur(30*i));
       remplirTriangle3d(surface, t20, echelle_de_couleur(30*i));
-
-      //printf("bula6\n");
       SDL_Delay(100);
 #endif
+
 #ifdef O3D
       /*
        * etape 3 : ecrire le corps des fonctions de lib_objet3d.c
@@ -80,9 +70,12 @@ int main(int argc,char** argv)
        * - finir par le tri des faces d'un objet et la composition des objets
        */
       vecteur = definirPoint3d(sin(i*M_PI/180),cos(i*M_PI/180),0);
-      translationObjet3d(o10, vecteur);
-      dessinerObjet3d(surface, o10);
+      //translationObjet3d(o10, vecteur);
+      //dessinerObjet3d(surface, o10);
+      dessinerObjet3d(surface, cube);
+      rotationObjet3d(cube,origine,0,5,0);
       free(vecteur);
+      SDL_Delay(500);
 #endif
 
       majEcran(surface);
